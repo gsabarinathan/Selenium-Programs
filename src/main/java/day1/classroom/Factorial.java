253f7ee4994386f4bfe0782a536c984d13f03d31
Sabarinathan G <Sabarinathan G@LAPTOP-7FJ43SJV>
2019-11-23 19:04:47 +0530

    First Day Programs

---
+++ src/main/java/day1/classroom/Factorial.java
@@ -0,0 +1,32 @@
+package day1.classroom;
+
+public class Factorial {
+	
+	/*
+	 * Goal: Find the Factorial of a given number
+	 * 
+	 * input: 5
+	 * output: 5*4*3*2*1 = 120
+	 * 
+	 * Shortcuts:
+	 * 1) Print : type: syso, followed by: ctrl + space + enter
+	 * 2) To create a 'for' loop: type 'for', followed by ctrl + space + down arrow + enter
+	 *   
+	 * What are my learnings from this code?
+	 * 1)
+	 * 2)
+	 * 3) 
+	 * 
+	 */	
+	
+	public static void main(String[] args) {
+			int fact=5 ;
+			int result=1;	
+		for(int i=1;i<=fact;i++)
+		{
+			result=result*i;			
+	}
+		System.out.print(result);
+	}
+	
+}
---
+++ src/main/java/day1/classroom/SwitchOperator.java
@@ -0,0 +1,78 @@
+package day1.classroom;
+
+public class SwitchOperator {
+
+	/*
+	 * Goal: Perform calculation based on the user input operations
+	 * 
+	 * inputs: 2,3 and "add"
+	 * output: added value: 5
+	 * 
+	 * Shortcuts:
+	 * 1) Print : type: syso, followed by: ctrl + space + enter
+	 * 2) To create a Switch case: type 'switch', followed by: ctrl + space + down arrow + enter
+	 *   
+	 * What are my learnings from this code?
+	 * 1)
+	 * 2)
+	 * 3) 
+	 * 
+	 */	
+	
+	public static void main(String[] args) {
+		
+		// Declare 2 integer numbers
+		
+ int a=5;
+ int b=10;
+ int c ;
+ String temp="div";
+		 switch (temp) {
+		case "add":
+			c=a+b;			
+			System.out.print("The Sum value is " +c);
+			break;
+			
+			case "sub":
+				c=a-b;			
+				System.out.print("The Difference value is " +c);
+				break;
+		
+			case "mul":
+				c=a*b;			
+				System.out.print("The Multiplied value is " +c);
+				break;
+				
+			case "div":
+				c=b/a;			
+				System.out.print("The Dividied value is " +c);
+				break;
+		default:
+			break;
+		}
+		// Declare a String variable with input as operations (Tip:
+		
+
+		// Initiate switch case for operations
+		
+		
+			// Within switch, include as case for add operation
+		
+		
+			// Within switch, include as case for sub operation
+		
+		
+			// Within switch, include as case for mul operation
+		
+		
+			// Within switch, include as case for div operation
+		
+		
+			// Within switch, include 'default' to handle other operations
+		
+		
+		//end of switch case
+		
+	}
+
+}
